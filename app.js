7b125b2831f12a087621511c409918a03b54a467
TheO.C.L.ProgramingGroupAdministrator <ip.no.254.203.0.8@gmail.com>
2019-09-05 19:56:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,127 +1,19 @@
 window.onload = function() {
 	var oauth = "YOUR_TOKEN";
 	var user = "YOUR_USERNAME"; // make sure this is all lowercase
-	if (window.reqName === undefined) {
-		window.reqName = window.prompt("Chat Name:");
-		window.reqName = window.reqName.toLowerCase();
-	};
-	// Get references to elements on the page.
-	var form = document.getElementById('message-form');
-	var messageField = document.getElementById('message');
-	var messagesList = document.getElementById('messages');
-	var socketStatus = document.getElementById('status');
-	var closeBtn = document.getElementById('close');
-
-
-	// Create a new WebSocket.
-	window.socket = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
-
-
-	// Handle any errors that occur.
-	socket.onerror = function(error) {
-		console.log('WebSocket Error: ' + error);
-	};
-
-
-	// Show a connected message when the WebSocket is opened.
-	socket.onopen = function(event) {
-		socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.URL;
-		socketStatus.className = 'open';
-		socket.send("CAP REQ :twitch.tv/tags twitch.tv/commands");
-		socket.send(`PASS oauth:${oauth}`);
-		socket.send(`NICK ${user}`);
-		socket.send(`USER ${ user } 8 * :${ user }`);
-		socket.send(`JOIN #${window.reqName}`);
-	};
-
-	window.message = function message(message, action) {
-		if (action === true) {
-			message = `ACTION ${message}`;
-		}
-		// Send the message through the WebSocket.
-		socket.send(`PRIVMSG #${ reqName } :` + message);
-
-		// Add the message to the messages list.
-		messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + "Message: " + message +
-			'</li>';
-
-		// Clear out the message field.
-		messageField.value = '';
-
-		return false;
-	};
-
-	// Handle messages sent by the server.
-	socket.onmessage = function(event) {
-		var msg = event.data;
-		messagesList.innerHTML += '<li class="received"><span>Received:</span>' +
-			msg + '</li>';
-		if (event.data == "PING :tmi.twitch.tv") {
-			socket.send("PONG");
-			messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + "PONG" +
-				'</li>';
-			console.log("PONG");
-		};
-		var VvV = msg.split(`PRIVMSG #${ window.reqName } :`);
-		switch (VvV[(VvV.length - 1)].toLowerCase().replace(decodeURIComponent("%0D%0A"), "")) {
-			case "!ping":
-				message("Pong!");
-				break;
-				/*
-				case "!test":
-					message("Test Complete! " + Math.floor(Math.random() * (1000 - 1 + 1) + 1)); // Needs random number to be able to not get caught by duplication... (I know this is a stupid fix)
-					break;
-				*/
-			case "!about":
-				message("Admin's Bot Project [V0.0.1] This is a Git-Hub Project created by YouTubeAdmin. This is a work in progress. :) " + Math.floor(Math.random() * (1000 - 1 + 1) + 1));
-				break;
-		}
-	};
-
-
-	// Show a disconnected message when the WebSocket is closed.
-	socket.onclose = function(event) {
-		socketStatus.innerHTML = 'Disconnected from WebSocket.';
-		socketStatus.className = 'closed';
-		window.onload();
-	};
-
-
-	// Send a message when the form is submitted.
-	form.onsubmit = function(e) {
-		e.preventDefault();
-
-		// Retrieve the message from the textarea.
-		var message = messageField.value;
-
-		// Send the message through the WebSocket.
-		socket.send(message);
-
-		// Add the message to the messages list.
-		messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
-			'</li>';
-
-		// Clear out the message field.
-		messageField.value = '';
-
-		return false;
-	};
-
-
-	// Close the WebSocket connection when the close button is clicked.
-	closeBtn.onclick = function(e) {
-		e.preventDefault();
-
-		// Close the WebSocket.
-		socket.close();
-
-		return false;
-	};
-};	var preName = getAllUrlParams().name;
+	var preName = getAllUrlParams().name;
 	var preCmds = getAllUrlParams().cmds;
 	if (preName !== undefined) {
 		window.reqName = decodeURIComponent(preName);
 	}
+	
+	var xml = new XMLHttpRequest();
+	xml.open("GET", "preferences.json");
+	xml.send();
+	xml.onreadystatechange = function (e) {
+		console.log(e);
+	};
+	
 	if (preCmds !== undefined) {
 		window.cmds = decodeURIComponent(preCmds);
 	}
@@ -139,8 +31,7 @@ window.onload = function() {
 
 	// Create a new WebSocket.
 	window.socket = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
-	window.cmds = [{"name": "-example", "return": "This is an example."}];
-
+	window.cmds = [{ "name": "-example", "return": "This is an example." }];
 
 	// Handle any errors that occur.
 	socket.onerror = function(error) {
@@ -148,33 +39,50 @@ window.onload = function() {
 	};
 
 	window.log = function log(message, pre, id) {
-		messagesList.innerHTML += `<li class="${id}"><span>${pre}</span>` + message + '</li>';
+		for (var i = 0; i; i++) {
+			if (message.includes("\n") || message.includes(decodeURI("%0A"))) {
+				message = message.replace(/\r|\n/g, "\\n");
+			}
+			else {
+				break;
+			}
+		}
+		if (messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1) {
+			messagesList.innerHTML += `<li class="${id}"><span>${pre}</span>` + message + '</li>';
+			messages.scrollTop = messages.scrollHeight;
+		}
+		else {
+			messagesList.innerHTML += `<li class="${id}"><span>${pre}</span>` + message + '</li>';
+		}
 	};
 
 	window.newKey = function() {
-		window.Session_Key = (Math.floor(Math.random() * (9990 - 1000 + 1) + 1000));
+		var tempKey = String(Math.floor(Math.random() * (9999 - 1000 + 1) + 1000)).split("");
+		tempKey = shuffle(tempKey);
+		window.Session_Key = Number(tempKey.join(""));
 		log(Session_Key, "Session Key:", "sent");
 		document.querySelector("#key").textContent = "Session Key: " + Session_Key;
 		console.log(Session_Key);
 	};
-	
+
 	window.checkKey = function(test) {
 		if (Number(test) === Session_Key) {
-			newKey();
 			return true;
-		} else {
+		}
+		else {
 			return false;
 		}
 	};
-	
-	window.sendError = function (message) {
-		message("/color red");
+
+	window.sendError = function(message, color = "red") {
+		message(`/color ${color}`);
 		message(message, true);
 		message("/color blue");
 	};
 
 	// Show a connected message when the WebSocket is opened.
 	socket.onopen = function(event) {
+		window.startTime = new Date().getTime();
 		socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.URL;
 		socketStatus.className = 'open';
 		socket.send("CAP REQ :twitch.tv/tags twitch.tv/commands");
@@ -194,13 +102,7 @@ window.onload = function() {
 
 		// Add the message to the messages list.
 		// console.log(messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1);
-		if (messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1) {
-			log(("Message: " + message), "Sent:", "sent");
-			messages.scrollTop = messages.scrollHeight;
-		}
-		else {
-			log(("Message: " + message), "Sent:", "sent");
-		}
+		log(("Message: " + message), "Sent:", "sent");
 
 		// Clear out the message field.
 		messageField.value = '';
@@ -209,26 +111,10 @@ window.onload = function() {
 	// Handle messages sent by the server.
 	socket.onmessage = function(event) {
 		var msg = event.data;
-		// console.log(messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1);
-		if (messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1) {
-			log(msg, "Received:", "received");
-			messages.scrollTop = messages.scrollHeight;
-		}
-		else {
-			log(msg, "Received:", "received");
-		}
+		log(msg, "Received:", "received");
 		if (String(msg).includes("PING")) {
-			if (messages.scrollHeight - messages.clientHeight <= messages.scrollTop + 1) {
-				socket.send("PONG");
-				log("PONG", "Sent:", "sent");
-				console.log("PONG");
-				messages.scrollTop = messages.scrollHeight;
-			}
-			else {
-				socket.send("PONG");
-				log("PONG", "Sent:", "sent");
-				console.log("PONG");
-			}
+			log("PONG", "Sent:", "sent");
+			console.log("PONG");
 		}
 		var VvV = msg.split(`PRIVMSG #${ window.reqName } :`);
 		VvV = VvV[(VvV.length - 1)].trimStart(); // Here VvV is left with only the sent chat string
@@ -315,21 +201,37 @@ window.onload = function() {
 					val = val.trim();
 					val = val.substring(val.indexOf(" "));
 					val = val.trim();
-				} else {
-					sendError("")
+				}
+				else {
+					sendError("You do not have access to this command.")
 					break;
 				}
 				var cmd = val.substring(0, val.indexOf(" "));
 				var say = val.substring(val.indexOf(" "));
-				cmds.push({"name": cmd, "return": say});
+				cmds.push({ "name": cmd, "return": say });
 				message(`The command ${cmd} has been successfully added!`);
 				break;
+			case "-uptime":
+				var seconds = new Date().getTime() - startTime;
+				var date = new Date(seconds * 100);
+				var hh = date.getUTCHours();
+				var mm = date.getUTCMinutes();
+				var ss = date.getSeconds();
+				// If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
+				// if (hh > 12) {hh = hh % 12;}
+				// These lines ensure you have two-digits
+				// if (hh < 10) { hh = "0" + hh; }
+				// if (mm < 10) { mm = "0" + mm; }
+				// if (ss < 10) { ss = "0" + ss; }
+				var t = hh + " hours, " + mm + " minutes, and " + ss + " seconds.";
+				message("Bot uptime is " + t);
 				// SECRET COMMANDS:
 			case "-shutdown":
-				if (Number(val) !== Session_Key) {
+				if (!checkKey(val)) {
 					sendError("You do not have access to this command.");
 					break;
 				}
+				newKey();
 				sendError("Shutting down...");
 				window.onload = function() {};
 				socket.close();
@@ -337,14 +239,14 @@ window.onload = function() {
 				console.log("%cThe session has ended.", 'color: rgb(255, 0, 0)');
 				break;
 			case "-clear":
-				if (Number(val) !== Session_Key) {
+				if (checkKey(val)) {
 					sendError("You do not have access to this command.");
 					break;
 				}
 				message("/clear");
 				newKey();
 				break;
-			// DEFAULT (for "hot loaded" commands)
+				// DEFAULT (for "hot loaded" commands)
 			default:
 				for (var i = 0; i < cmds.length; i++) {
 					if (cmds[i].name == String(calr)) {
@@ -374,7 +276,7 @@ window.onload = function() {
 		if (message.toLowerCase().includes("key")) {
 			newKey();
 		};
-		
+
 		socket.send(message);
 
 		// Add the message to the messages list.
